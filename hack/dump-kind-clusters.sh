d33af69c62b2d04074467f24fe80106870fd1561
xrstf <git@xrstf.de>
2023-09-02 01:43:00 +0200

    MOAR DATA

--- hack/dump-kind-clusters.sh
+++ hack/dump-kind-clusters.sh
@@ -11,7 +11,7 @@ export KUBECONFIG="$(realpath kubeconfig)"
 
 clusterName=kubernetes-apis
 
-for release in 1.28; do
+for release in 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.22 1.23 1.24 1.25 1.26 1.27 1.28; do
   echo "Dumping APIs for Kubernetes $release …"
 
   image="$(jq --arg rel "$release" -r '.[$rel]' hack/kind-images.json)"
