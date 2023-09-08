9d017a6402bb1ff59a8c7e580b15d1bd1eefefb9
xrstf <git@xrstf.de>
2023-09-08 22:45:34 +0200

    regenerate all docs

--- hack/generate-apidocs.sh
+++ hack/generate-apidocs.sh
@@ -4,8 +4,22 @@ set -e
 
 cd $(dirname $0)/..
 
+export RELEASES="1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.23 1.24 1.25 1.26 1.27 1.28 1.29"
+
 docker run \
   --rm \
-  -e "RELEASES=1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.23 1.24 1.25 1.26 1.27 1.28 1.29" \
+  -e "RELEASES=$RELEASES" \
   -v "$(realpath public/apidocs/):/output" \
   kubernetes-apidocs:latest /make.sh
+
+cd public/apidocs/
+for release in $RELEASES; do
+  cd "$release"
+
+  sed -i 's#href="favicon.ico" type="image/vnd.microsoft.icon"#href="../static/images/favicon.png" type="image/png"#g' index.html
+  sed -i 's#"/css/#"../static/css/#g' index.html
+  sed -i 's#"/js/#"../static/js/#g' index.html
+  sed -i 's#"js/navData.js"#"navData.js"#g' index.html
+
+  cd ..
+done
