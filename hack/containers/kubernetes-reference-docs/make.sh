e15361a59f4fc60b95f881fa11be47ba1c3fa69d
xrstf <git@xrstf.de>
2023-09-17 10:53:50 +0200

    make apidocs easier to lint

--- hack/containers/kubernetes-reference-docs/make.sh
+++ hack/containers/kubernetes-reference-docs/make.sh
@@ -10,5 +10,13 @@ for release in $RELEASES; do
   mkdir -p "/output/$release"
 
   K8S_RELEASE=$release make api
-  cp gen-apidocs/build/index.html "/output/$release/"
+
+  # beautify HTML / make validations easier by preventing
+  # loads of info messages on https://validator.w3.org/nu/
+  index=gen-apidocs/build/index.html
+
+  sed -i 's#<BR />#<BR>#gi' $index
+  sed -i 's#<BR/>#<BR>#gi' $index
+
+  cp $index "/output/$release/"
 done
