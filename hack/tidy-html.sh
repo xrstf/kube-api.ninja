fd568126a6455cd1ca53e0bdd0ba43bc6af119cd
Christoph Mewes <christoph@kubermatic.com>
2024-09-29 16:19:35 +0200

    also trim trailing space in regular html files

--- hack/tidy-html.sh
+++ hack/tidy-html.sh
@@ -6,4 +6,8 @@ cd $(dirname $0)/..
 cd public
 for file in *.html; do
   tidy -config ../tidy.conf -modify "$file"
+
+  # trim trailing whitespace added by tidy
+  # cf. https://github.com/htacg/tidy-html5/issues/523
+  sed -i 's/[[:blank:]]\+$//g' "$file"
 done
